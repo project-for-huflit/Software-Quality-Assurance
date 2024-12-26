6f78311db4cfba0e40d72c15d2ed559b2ae134f0
quan3473 <nguyenminhquan042004@gmail.com>
2024-12-26 20:37:01 +0700

    Change src mobile

---
+++ mobile/android/app/src/main/kotlin/com/io/trackio/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.io.trackio.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mobile/lib/utils/router.dart
@@ -0,0 +1,22 @@
+// import 'package:auto_route/auto_route.dart';
+
+// @MaterialAutoRouter(
+//   routes: <AutoRoute>[
+//     AutoRoute(
+//       path: '/',
+//       page: AppWrapper,
+//       initial: true,
+//       children: [
+//         AutoRoute(
+//           path: 'login',
+//           page: LoginScreen,
+//         ),
+//         AutoRoute(
+//           path: 'home',
+//           page: AppNavigator,
+//         ),
+//       ],
+//     ),
+//   ],
+// )
+class $AppRouter {}
