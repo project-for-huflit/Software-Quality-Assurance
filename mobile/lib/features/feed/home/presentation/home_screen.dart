4e8ff0c0e06ad8d898e1b20de71a15ba24b93d55
trungenglish <nguyentrung23232323@gmail.com>
2025-02-05 17:30:25 +0700

    add something

--- mobile/lib/features/feed/home/presentation/home_screen.dart
+++ mobile/lib/features/feed/home/presentation/home_screen.dart
@@ -1,38 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/features/app/widgets/navigation/bottom_navigation.dart';
 import 'package:mobile/features/feed/home/widgets/app_bar.dart';
 import 'package:mobile/features/feed/home/widgets/bottom_nav_bar.dart';
+import 'package:mobile/features/feed/home/widgets/top_expense.dart';
 import 'package:mobile/features/feed/home/widgets/total_balance.dart';
+import 'package:mobile/features/feed/home/widgets/overview_report.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavBarWidget(),
-        backgroundColor: Colors.white,
-        body: Container(
-          alignment: Alignment.centerLeft,
-          child: Column(
+      bottomNavigationBar: BottomNavBarWidget(),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 1.5, // Đảm bảo Stack đủ lớn để cuộn
+          child: Stack(
             children: [
-              Expanded(
-                child: Stack(
-                  children: [
-                    AppBarWidget(),
-                    Positioned(
-                      top: 140,
-                      left: 20,
-                      right: 20,
-                      child:
-                        TotalBalanceWidget(),
-                    ),
-                  ]
-              ),),
+              AppBarWidget(),
+              Positioned(
+                top: 140,
+                left: 20,
+                right: 20,
+                child: TotalBalanceWidget(),
+              ),
+              Positioned(
+                top: 300,
+                left: 20,
+                right: 20,
+                child: OverViewReport(),
+              ),
+              Positioned(
+                top: 680,
+                left: 20,
+                right: 20,
+                child: TopExpense(),
+              ),
             ],
           ),
-        )
+        ),
+      ),
     );
   }
 }
-
